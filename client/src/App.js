3509f73402c4e750958132e2f0ecaf4cc25a981c
mojsun <m.khorashahi7@gmail.com>
2024-07-24 19:15:02 -0400

    Update server configuration and Vercel settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,25 @@
+// import AppRoutes from "./routes/AppRoutes.jsx";
+// import { InMemoryCache, ApolloClient } from "@apollo/client";
+// import { QueryProvider } from "./utils/QueryProvider.jsx";
+// import "./App.css";
+// import "./App.scss";
+
+// const client = new ApolloClient({
+//   uri: "https://app-sportt-api.vercel.app.vercel.app/graphql",
+//   cache: new InMemoryCache(),
+//   credentials: "include", // If you're using cookies for authentication
+// });
+// function App() {
+//   return (
+//     <QueryProvider>
+//       <AppRoutes />
+//     </QueryProvider>
+//   );
+// }
+
+// export default App;
+
+import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
 import AppRoutes from "./routes/AppRoutes.jsx";
 import { QueryProvider } from "./utils/QueryProvider.jsx";
 import "./App.css";
@@ -8,11 +30,14 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
   credentials: "include", // If you're using cookies for authentication
 });
+
 function App() {
   return (
-    <QueryProvider>
-      <AppRoutes />
-    </QueryProvider>
+    <ApolloProvider client={client}>
+      <QueryProvider>
+        <AppRoutes />
+      </QueryProvider>
+    </ApolloProvider>
   );
 }
 
